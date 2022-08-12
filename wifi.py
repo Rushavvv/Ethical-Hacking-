2fff35a707c65a7501782d39d396be808a6f8ed8
Rushav <rusu.sthapit@gmail.com>
2022-08-12 16:48:00 +0545

    :hammer: Wifi passwords

--- wifi.py
+++ wifi.py
@@ -1,4 +1,35 @@
-import hashlib
+# import hashlib
 
-pass_hash = input("Enter md5 hash: ") 
-wordlist = input("File name: ")
+# flag = 0 
+
+
+# pass_hash = input("Enter md5 hash: ") 
+# wordlist = input("File name: ")
+
+# try: 
+#     pass_file = open(wordlist, "r") 
+# except: 
+#     print("No file found") 
+#     quit() 
+
+
+# for word in pass_file: 
+#     enc_word = word.encode('utf-8') 
+#     digest = hashlib.md5(enc_word.strip()).hexdigest()
+
+#     if digest == pass_hash: 
+#         print("Password has been found") 
+#         print("Password is" + word)
+#         flag = 1  
+#         break 
+
+# if flag ==0: 
+#     print("Password is not in the list") 
+
+import subprocess
+
+
+import re 
+
+wifi_name = input("Enter wifi name: ")
+command = subprocess.run (["security" , "find-generic-password" , "-wa", f"{wifi_name}"])
